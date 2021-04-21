1b5c949f2f16897f65155b1deacb9d160b7a4c32
Leo Volpe <Leo Volpe@LAPTOP-VU9KPGVR.station>
2021-04-21 13:35:49 +0200

    piu performante

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -1,16 +1,111 @@
 package it.polito.tdp.nobel.model;
 
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
+
+import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 
+	private List<Esame> partenza;
+	private Set<Esame> soluzMigliore;
+	private double mediaSoluzMigl;
+	private int castiTestati;
+	
+	public Model() {
+		EsameDAO dao= new EsameDAO();
+		this.partenza= dao.getTuttiEsami();
+		this.castiTestati=0;
+	}
 	
 	public Set<Esame> calcolaSottoinsiemeEsami(int numeroCrediti) {
+		Set<Esame> parziale = new HashSet<Esame>();
+		this.soluzMigliore = new HashSet<Esame>();
+		this.mediaSoluzMigl=0;
+		//cerca1(parziale,0,numeroCrediti);
+		cerca2(parziale,0,numeroCrediti);
 		System.out.println("TODO!");
-		return null;	
+		return this.soluzMigliore;	
 	}
 
+	//complessita' 2^N
+	private void cerca2(Set<Esame> parziale, int L, int m) {
+		this.castiTestati++;
+		//casi terminali:
+		int crediti= sommaCrediti(parziale);
+		if (crediti>m) {
+			return;
+		}
+		if (crediti==m) {
+			double media= calcolaMedia(parziale);
+			if (media >this.mediaSoluzMigl ) {
+			this.soluzMigliore= new LinkedHashSet<Esame>(parziale);
+			this.mediaSoluzMigl=media;
+			}
+			return;
+		}
+		//sicuramente crediti < m se arrivo a questo punto
+		//L==N -> non ci sono piu' esami da aggiungere
+		if (L==partenza.size()) {
+			return; //non ho piu' esami, ma non ho raggiunto i crediti sufficenti
+		}
+		
+		//generazioni sotto-problemi
+		//partenza[L] e' da aggiunggere o no?
+		parziale.add(partenza.get(L));
+		cerca2(parziale,L+1,m);
+		parziale.remove(partenza.get(L));
+		cerca2(parziale,L+1,m);
+		
+	}
+
+	//complessita' N!
+	private void cerca1(Set<Esame> parziale, int L, int m) {
+		this.castiTestati++;
+		// System.out.println("L = " + L + "\t" + parziale);  per stampare via via le soluzioni parziali
+		//casi terminali:
+		int crediti= sommaCrediti(parziale);
+		if (crediti>m) {
+			return;
+		}
+		if (crediti==m) {
+			double media= calcolaMedia(parziale);
+			if (media >this.mediaSoluzMigl ) {
+				this.soluzMigliore= new HashSet<Esame>(parziale);
+				this.mediaSoluzMigl=media;
+			}
+			return;
+		}
+		//sicuramente crediti < m se arrivo a questo punto
+		//L==N -> non ci sono piu' esami da aggiungere
+		if (L==partenza.size()) {
+			return; //non ho piu' esami, ma non ho raggiunto i crediti sufficenti
+		}
+		
+		/* //genero i sotto-problemi: //poco efficente
+		for (Esame e : partenza) {
+			if (!parziale.contains(e)) {
+				parziale.add(e);
+				cerca1(parziale,L+1,m);
+				parziale.remove(e);
+			}
+		}*/
+		for (int i=0; i<partenza.size(); i++) {  //leggermente piu efficente
+			if (!parziale.contains(partenza.get(i)) && i>=L) {
+				parziale.add(partenza.get(i));
+				cerca1(parziale,L+1,m);
+				parziale.remove(partenza.get(i));
+			}
+		}
+		
+	}
 	
+	public int getCasiTestati() {
+		return this.castiTestati;
+	}
+
 	public double calcolaMedia(Set<Esame> esami) {
 		
 		int crediti = 0;
